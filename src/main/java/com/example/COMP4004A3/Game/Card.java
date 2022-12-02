849808df38d6844980bee15d126230643d797ccf
BuildTools <unconfigured@null.spigotmc.org>
2022-12-02 13:24:10 -0500

    Updated Card with toString and equals methods. Added css for custom styling if need be

--- src/main/java/com/example/COMP4004A3/Game/Card.java
+++ src/main/java/com/example/COMP4004A3/Game/Card.java
@@ -15,8 +15,40 @@ public class Card {
     }
 
 
-    private Suit suit;
+    private final Suit suit;
 
     //1-13, 1 is ACE, 11 is JACK, 12 is QUEEN, 13 is KING
-    private int rank;
+    private final int rank;
+
+    public final int getRank() {
+        return rank;
+    }
+
+    public final Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public boolean equals(final Object card){
+        return card instanceof Card &&((Card) card).getRank() == this.rank && ((Card) card).getSuit() == this.suit;
+    }
+
+    @Override
+    public String toString(){
+        switch (suit) {
+            case HEART:
+                return "H" + rank;
+
+            case DIAMOND:
+                return "D" + rank;
+
+            case SPADE:
+                return "S" + rank;
+
+            case CLUB:
+                return "C" + rank;
+
+        }
+        return "AS";
+    }
 }
