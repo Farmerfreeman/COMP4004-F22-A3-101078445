950546900343265ca3454156821e3a1f935c8a84
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 16:03:10 -0500

    U-TEST COMMIT: AddPlayer() tests addPLayer fcn in game class. Will only be used for unit testing player interactions, since otherwise the register player will handle adding players using a networking session.

--- src/main/test/java/UnitTests.java
+++ src/main/test/java/UnitTests.java
@@ -1,14 +1,8 @@
 
 
-import com.example.COMP4004A3.Game.Card;
-import com.example.COMP4004A3.Game.discardPile;
-import com.example.COMP4004A3.Game.stockPile;
-import org.checkerframework.checker.units.qual.C;
+import com.example.COMP4004A3.Game.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.*;
-import java.util.Dictionary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,14 +11,14 @@ public class UnitTests {
     @Test
     @DisplayName("Test StockPile init: Pile has 52 Cards")
     void initStockPile(){
-        stockPile p = new stockPile();
+        StockPile p = new StockPile();
         assertEquals(52, p.getCards().size());
     }
 
     @Test
     @DisplayName("Draw a card from stockpile")
     void drawFromStock(){
-        stockPile p = new stockPile();
+        StockPile p = new StockPile();
         Card c = p.drawCard();
         assertTrue(c.getClass() == Card.class);
     }
@@ -32,7 +26,7 @@ public class UnitTests {
     @Test
     @DisplayName("Draw a card from stockpile when none are left")
     void drawFromEmptyStock(){
-        stockPile p = new stockPile();
+        StockPile p = new StockPile();
         Card c = new Card();
         for (int i = 0; i<=52; i++){
             c = p.drawCard();
@@ -44,10 +38,28 @@ public class UnitTests {
     @Test
     @DisplayName("Add a card to the Discard Pile")
     void discardCard(){
-        discardPile p = new discardPile();
+        DiscardPile p = new DiscardPile();
         Card c = new Card();
         p.addCard(c);
 
         assertEquals(c, p.getCards().get(0));
+    }
+
+    @Test
+    @DisplayName("Add a player to Game (Only used for testing)")
+    void addPlayer(){
+        Game g = new Game();
+        g.init();
+
+        Player p = new Player("p1");
+        g.addPlayer(p);
+
+        assertTrue(g.getPlayers().contains(p));
+    }
+
+    @Test
+    @DisplayName("Play a Card (Player Hand to Discard Pile)")
+    void playCard(){
+
     }
 }
