3bd31681f23ba593ab82da018857ea2b423dbbb2
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 18:56:13 -0500

    U-TEST COMMIT: Added canMultiplePlayersConnect test which tests all 4 players connecting and game starting properly. Test Passes

--- src/main/test/java/com/example/COMP4004A3/cucumber/config/selenium/ConnectionTest.java
+++ src/main/test/java/com/example/COMP4004A3/cucumber/config/selenium/ConnectionTest.java
@@ -1,11 +1,16 @@
 package com.example.COMP4004A3.cucumber.config.selenium;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertTrue;
 
 public class ConnectionTest extends SeleniumTest {
     @Test
@@ -15,5 +20,35 @@ public class ConnectionTest extends SeleniumTest {
         assertThat(this.indexPage.hasText("You are now connected with id"), is(true));
     }
 
+    @Test
+    public void canMultiplePlayersConnect(){
 
+
+        final WebDriver player1 = this.quickConnectSecondUser();
+
+        this.delay(1);
+
+        final WebDriver player2 = this.quickConnectSecondUser();
+
+        this.delay(1);
+
+        final WebDriver player3 = this.quickConnectSecondUser();
+
+        this.delay(1);
+
+        final WebDriver player4 = this.quickConnectSecondUser();
+
+        this.delay(1);
+
+        String consoleText = "";
+
+        WebElement console = player1.findElement(By.id("console"));
+        List<WebElement> children = console.findElements(By.xpath("./child::*"));
+
+        for (WebElement c : children){
+            consoleText += c.getText();
+        }
+
+        assertTrue(consoleText.contains("The game has started! Please wait your turn"));
+    }
 }
